eb28139d1d180216fe662e66b47c825f7288b67b
SohelShahid117 <sohelshahid09@gmail.com>
2024-10-02 23:01:52 +0600

    done upto 58-3 Style Header and create simple banner

---
+++ car-doctor-client/src/main.jsx
@@ -0,0 +1,33 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Main from "./Layout/Main";
+import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    // element: <div>Hello world!</div>,
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <div className="max-w-7xl mx-auto">
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </div>
+);
